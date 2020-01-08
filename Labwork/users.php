41863ceac865b6f7bb2033b624fc96b771596f16
Fran2-hash <55898543+Fran2-hash@users.noreply.github.com>
2020-01-08 04:51:18 -0800

    Add files via upload

---
+++ Labwork/users.php
@@ -0,0 +1,23 @@
+<?php
+
+include 'connect.php';
+if(!isset(($_POST['search']))||($_POST['search'] ===""))
+{
+	$sql = "select firstname, surname, email from livesearch ORDER BY surname ASC";
+}
+else
+{
+	$search = $_POST['search'];
+    $sql = "select firstname, surname, email from livesearch where firstname like '%$search%' or surname like '%$search%' ORDER BY surname ASC";
+    
+ }
+$query = $mysqli->query($sql); //$conn is defined in connect.php
+    $users = array();
+        while($row = $query->fetch_array()){
+	    array_push($users, $row);
+                    }
+        $mysqli->close();
+header("Content-type: application/json");
+echo json_encode($users);
+die();
+?>
